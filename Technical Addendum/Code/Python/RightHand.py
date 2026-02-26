f5d6863ce0c07a7809aa08e15339270091853440
Snake54310 <86499062+Snake54310@users.noreply.github.com>
2026-02-25 18:43:14 -0700

    live animation functioning as much as filtered sensor data will allow, next will add more options for leveling and see if an AI can be trained to estimate drift based on sensor data history.

--- Technical Addendum/Code/Python/RightHand.py
+++ Technical Addendum/Code/Python/RightHand.py
@@ -6,6 +6,9 @@ class Thumb:
         self.seg1Len = segment1Length
         self.seg2Len = segment2Length
         self.j1flex = 0.0 # angle of joint between segments 1 and 2
+        self.orientationX = 0 #270
+        self.orientationY = 0
+        self.orientationZ = 0
 
     def setJ1Flex(self, newJ1Flex):
         self.j1flex = newJ1Flex
@@ -16,7 +19,20 @@ class Thumb:
 
     def getSegLens(self):
         return [self.seg1Len, self.seg2Len]
+    def setOrientation(self, X, Y, Z): # NOTE: this orientation is RELATIVE to wrist. Absolute orientation is stored in hand class
+        self.orientationX = X
+        self.orientationY = Y
+        self.orientationZ = Z
+        return
 
+    def getOrientation(self):
+        return [self.orientationX, self.orientationY, self.orientationZ]
+
+    def zeroOrientation(self):
+        self.orientationX = 0 #270
+        self.orientationY = 0
+        self.orientationZ = 0
+        return
 
 class Pointer:
     def __init__(self, segment1Length, segment2Length, segment3Length): # segments progressive from base
@@ -25,6 +41,9 @@ class Pointer:
         self.seg3Len = segment3Length
         self.j1flex = 0.0  # angle of joint between segments 1 and 2
         self.j2flex = 0.0  # angle of joint between segments 2 and 3
+        self.orientationX = 0 #270
+        self.orientationY = 0
+        self.orientationZ = 0
 
     def setJ1Flex(self, newJ1Flex):
         self.j1flex = newJ1Flex
@@ -42,6 +61,22 @@ class Pointer:
 
     def getSegLens(self):
         return [self.seg1Len, self.seg2Len, self.seg3Len]
+
+    def setOrientation(self, X, Y,
+                       Z):  # NOTE: this orientation is RELATIVE to wrist. Absolute orientation is stored in hand class
+        self.orientationX = X
+        self.orientationY = Y
+        self.orientationZ = Z
+        return
+
+    def getOrientation(self):
+        return [self.orientationX, self.orientationY, self.orientationZ]
+
+    def zeroOrientation(self):
+        self.orientationX = 0 #270
+        self.orientationY = 0
+        self.orientationZ = 0
+        return
 
 
 class Middle:
@@ -51,6 +86,9 @@ class Middle:
         self.seg3Len = segment3Length
         self.j1flex = 0.0  # angle of joint between segments 1 and 2
         self.j2flex = 0.0  # angle of joint between segments 2 and 3
+        self.orientationX = 0 #270
+        self.orientationY = 0
+        self.orientationZ = 0
 
     def setJ1Flex(self, newJ1Flex):
         self.j1flex = newJ1Flex
@@ -68,6 +106,22 @@ class Middle:
 
     def getSegLens(self):
         return [self.seg1Len, self.seg2Len, self.seg3Len]
+
+    def setOrientation(self, X, Y,
+                       Z):  # NOTE: this orientation is RELATIVE to wrist. Absolute orientation is stored in hand class
+        self.orientationX = X
+        self.orientationY = Y
+        self.orientationZ = Z
+        return
+
+    def getOrientation(self):
+        return [self.orientationX, self.orientationY, self.orientationZ]
+
+    def zeroOrientation(self):
+        self.orientationX = 0 #270
+        self.orientationY = 0
+        self.orientationZ = 0
+        return
 
 
 class Ring:
@@ -77,6 +131,9 @@ class Ring:
         self.seg3Len = segment3Length
         self.j1flex = 0.0  # angle of joint between segments 1 and 2
         self.j2flex = 0.0  # angle of joint between segments 2 and 3
+        self.orientationX = 0 #270
+        self.orientationY = 0
+        self.orientationZ = 0
 
     def setJ1Flex(self, newJ1Flex):
         self.j1flex = newJ1Flex
@@ -94,6 +151,22 @@ class Ring:
 
     def getSegLens(self):
         return [self.seg1Len, self.seg2Len, self.seg3Len]
+
+    def setOrientation(self, X, Y,
+                       Z):  # NOTE: this orientation is RELATIVE to wrist. Absolute orientation is stored in hand class
+        self.orientationX = X
+        self.orientationY = Y
+        self.orientationZ = Z
+        return
+
+    def getOrientation(self):
+        return [self.orientationX, self.orientationY, self.orientationZ]
+
+    def zeroOrientation(self):
+        self.orientationX = 0 #270
+        self.orientationY = 0
+        self.orientationZ = 0
+        return
 
 
 class Pinky:
@@ -103,6 +176,9 @@ class Pinky:
         self.seg3Len = segment3Length
         self.j1flex = 0.0  # angle of joint between segments 1 and 2
         self.j2flex = 0.0  # angle of joint between segments 2 and 3
+        self.orientationX = 0 #270
+        self.orientationY = 0
+        self.orientationZ = 0
 
     def setJ1Flex(self, newJ1Flex):
         self.j1flex = newJ1Flex
@@ -121,6 +197,22 @@ class Pinky:
     def getSegLens(self):
         return [self.seg1Len, self.seg2Len, self.seg3Len]
 
+    def setOrientation(self, X, Y,
+                       Z):  # NOTE: this orientation is RELATIVE to wrist. Absolute orientation is stored in hand class
+        self.orientationX = X
+        self.orientationY = Y
+        self.orientationZ = Z
+        return
+
+    def getOrientation(self):
+        return [self.orientationX, self.orientationY, self.orientationZ]
+
+    def zeroOrientation(self):
+        self.orientationX = 0 #270
+        self.orientationY = 0
+        self.orientationZ = 0
+        return
+
 
 class RightHand: # NOTE: if two distinct hand classes are not necessary, backtrack and re-write this for the general case
     def __init__(self):
@@ -129,10 +221,32 @@ class RightHand: # NOTE: if two distinct hand classes are not necessary, backtra
         self.middle = Middle(2.5, 1.375, 1.0)
         self.ring = Ring(2.25, 1.375, 1.0)
         self.pinky = Pinky(1.375, 1.0, 1.0)
+
         self.wristGyroDegX = 0
         self.wristGyroDegY = 0
         self.wristGyroDegZ = 0
         self.sampleRate = 10
+
+        # NOTE: THESE ARE ABSOLUTE ROTATION ORIENTATION FOR FINGERS
+        self.thumbGyroX = 0
+        self.thumbGyroY = 0
+        self.thumbGyroZ = 0
+
+        self.pointerGyroX = 0
+        self.pointerGyroY = 0
+        self.pointerGyroZ = 0
+
+        self.middleGyroX = 0
+        self.middleGyroY = 0
+        self.middleGyroZ = 0
+
+        self.ringGyroX = 0
+        self.ringGyroY = 0
+        self.ringGyroZ = 0
+
+        self.pinkyGyroX = 0
+        self.pinkyGyroY = 0
+        self.pinkyGyroZ = 0
 
     def setJ1Angles(self, thumbFlex, pointerFlex, middleFlex, ringFlex, pinkyFlex):
         self.thumb.setJ1Flex(thumbFlex)
@@ -159,21 +273,151 @@ class RightHand: # NOTE: if two distinct hand classes are not necessary, backtra
         self.sampleRate = newRate
         return
     def updateOrientation(self, wristGyroRadssX, wristGyroRadssY, wristGyroRadssZ):
-        self.wristGyroDegX = (self.wristGyroDegX + ((wristGyroRadssX / self.sampleRate) * 180 / math.pi) + 360) % 360
-        self.wristGyroDegY = (self.wristGyroDegY + ((wristGyroRadssY / self.sampleRate) * 180 / math.pi) + 360) % 360
-        self.wristGyroDegZ = (self.wristGyroDegZ + ((wristGyroRadssZ / self.sampleRate) * 180 / math.pi) + 360) % 360
+        # NOTE: SENSOR X_rotation on wrist is in -X direction in animation window (e.g. 340 degrees sensor => 20 degrees animation).
+        # NOTE: Sensor Y and Z rotations are swapped
+        # NOTE: After swapping Sensor Y and Z with window Z and Y, Sensor data for Z in window is reversed (-Z direction)
+
+        self.wristGyroDegX = (self.wristGyroDegX - ((wristGyroRadssX / self.sampleRate) * 180 / math.pi) + 360) % 360
+        self.wristGyroDegY = (self.wristGyroDegY + ((wristGyroRadssZ / self.sampleRate) * 180 / math.pi) + 360) % 360
+        self.wristGyroDegZ = (self.wristGyroDegZ - ((wristGyroRadssY / self.sampleRate) * 180 / math.pi) + 360) % 360
+        return
+
+    def updateOrientationFingers(self, thumbX, thumbY, thumbZ, pointerX, pointerY, pointerZ,
+                                 middleX, middleY, middleZ, ringX, ringY, ringZ, pinkyX, pinkyY, pinkyZ):
+        self.thumbGyroX = (self.thumbGyroX - ((thumbY / self.sampleRate) * 180 / math.pi) + 360) % 360
+        self.thumbGyroY = (self.thumbGyroY + ((thumbX / self.sampleRate) * 180 / math.pi) + 360) % 360
+        self.thumbGyroZ = (self.thumbGyroZ + ((thumbZ / self.sampleRate) * 180 / math.pi) + 360) % 360
+
+        self.pointerGyroX = (self.pointerGyroX - ((pointerY / self.sampleRate) * 180 / math.pi) + 360) % 360
+        self.pointerGyroY = (self.pointerGyroY + ((pointerX / self.sampleRate) * 180 / math.pi) + 360) % 360
+        self.pointerGyroZ = (self.pointerGyroZ + ((pointerZ / self.sampleRate) * 180 / math.pi) + 360) % 360
+
+        self.middleGyroX = (self.middleGyroX - ((middleY / self.sampleRate) * 180 / math.pi) + 360) % 360
+        self.middleGyroY = (self.middleGyroY + ((middleX / self.sampleRate) * 180 / math.pi) + 360) % 360
+        self.middleGyroZ = (self.middleGyroZ + ((middleZ / self.sampleRate) * 180 / math.pi) + 360) % 360
+
+        self.ringGyroX = (self.ringGyroX - ((ringY / self.sampleRate) * 180 / math.pi) + 360) % 360
+        self.ringGyroY = (self.ringGyroY + ((ringX / self.sampleRate) * 180 / math.pi) + 360) % 360
+        self.ringGyroZ = (self.ringGyroZ + ((ringZ / self.sampleRate) * 180 / math.pi) + 360) % 360
+
+        self.pinkyGyroX = (self.pinkyGyroX - ((pinkyY / self.sampleRate) * 180 / math.pi) + 360) % 360
+        self.pinkyGyroY = (self.pinkyGyroY + ((pinkyX / self.sampleRate) * 180 / math.pi) + 360) % 360
+        self.pinkyGyroZ = (self.pinkyGyroZ + ((pinkyZ / self.sampleRate) * 180 / math.pi) + 360) % 360
+
+        thumbRelativeOrienation = [((self.thumbGyroX - self.wristGyroDegX) + 360) % 360,
+                                   ((self.thumbGyroY + self.wristGyroDegZ) + 360) % 360,
+                                   ((self.thumbGyroZ - self.wristGyroDegY) + 360) % 360]
+
+        self.thumb.setOrientation(thumbRelativeOrienation[0], thumbRelativeOrienation[1], thumbRelativeOrienation[2])
+
+        self.thumb.setOrientation(thumbRelativeOrienation[0], thumbRelativeOrienation[1], thumbRelativeOrienation[2])
+
+        pointerRelativeOrienation = [((self.pointerGyroX - self.wristGyroDegX) + 360) % 360, # self.pointerGyroX - self.wristGyroDegX
+                                     ((self.pointerGyroY + self.wristGyroDegZ) + 360) % 360, # self.pointerGyroY + self.wristGyroDegZ
+                                   #((self.pointerGyroY + ((self.wristGyroDegZ + 180) % 360 - 180) * math.sqrt(3) / 2 + ((self.wristGyroDegY + 180) % 360 - 180) * 1 / 2) + 360) % 360, # self.pointerGyroY + self.wristGyroDegZ
+                                     ((self.pointerGyroZ - self.wristGyroDegY) + 360) % 360] # self.pointerGyroZ - self.wristGyroDegY
+
+        self.pointer.setOrientation(pointerRelativeOrienation[0], pointerRelativeOrienation[1], pointerRelativeOrienation[2])
+
+        middleRelativeOrienation = [((self.middleGyroX - self.wristGyroDegX) + 360) % 360,
+                                    ((self.middleGyroY + self.wristGyroDegZ) + 360) % 360,
+                                    ((self.middleGyroZ - self.wristGyroDegY) + 360) % 360]
+
+        self.middle.setOrientation(middleRelativeOrienation[0], middleRelativeOrienation[1],
+                                   middleRelativeOrienation[2])
+
+        ringRelativeOrienation = [((self.ringGyroX - self.wristGyroDegX) + 360) % 360,
+                                  ((self.ringGyroY + self.wristGyroDegZ) + 360) % 360,
+                                  ((self.ringGyroZ - self.wristGyroDegY) + 360) % 360]
+
+        self.ring.setOrientation(ringRelativeOrienation[0], ringRelativeOrienation[1], ringRelativeOrienation[2])
+
+        pinkyRelativeOrienation = [((self.pinkyGyroX - self.wristGyroDegX) + 360) % 360,
+                                   ((self.pinkyGyroY + self.wristGyroDegZ) + 360) % 360,
+                                   ((self.pinkyGyroZ - self.wristGyroDegY) + 360) % 360]
+
+        self.pinky.setOrientation(pinkyRelativeOrienation[0], pinkyRelativeOrienation[1], pinkyRelativeOrienation[2])
+
         return
 
     def zeroOrientation(self):
         self.wristGyroDegX = 0
         self.wristGyroDegY = 0
         self.wristGyroDegZ = 0
+        self.thumb.zeroOrientation()
+        self.pointer.zeroOrientation()
+        self.middle.zeroOrientation()
+        self.ring.zeroOrientation()
+        self.pinky.zeroOrientation()
+
+        self.thumbGyroX = 0
+        self.thumbGyroY = 0
+        self.thumbGyroZ = 0
+
+        self.pointerGyroX = 0
+        self.pointerGyroY = 0
+        self.pointerGyroZ = 0
+
+        self.middleGyroX = 0
+        self.middleGyroY = 0
+        self.middleGyroZ = 0
+
+        self.ringGyroX = 0
+        self.ringGyroY = 0
+        self.ringGyroZ = 0
+
+        self.pinkyGyroX = 0
+        self.pinkyGyroY = 0
+        self.pinkyGyroZ = 0
         return
 
     def getOrientation(self):
-        return [self.wristGyroDegX, self.wristGyroDegY, self.wristGyroDegZ]
+        return [self.wristGyroDegX + 90, self.wristGyroDegY, self.wristGyroDegZ]
 
+    def getRelativeOrientations(self):
+        relativeOrientations = (self.thumb.getOrientation() + self.pointer.getOrientation() +
+                               self.middle.getOrientation() + self.ring.getOrientation() + self.pinky.getOrientation())
+        return relativeOrientations
 
+    def getJ0Angles(self):
+        # all of this math is deceivingly simple and the proof involves:
+        # 1. making the assumption that the finger's 2 forward joints (1 in the case of the thumb)
+        # all rotate around 1 axis and thus lie on one plane
+        # 2. finding this plane using the X, Y, Z orientation of the distal segment and the reversed-rotation axis
+        # of the 2 forward joints (1 in the case of the thumb) all relative to the angle of the back of the hand (wrist)
+        # 3. reverse-transforming this rotation inside of the finger's plane to get the rotation of the base segment
+        # around the finger's rotation axis
+        # 4. converting this rotation back from the finger's plane to the domain where we can determine a base-segment
+        # angle relative to the wrist's plane (assuming the wrist runs forward along the Y-axis for simplicity).
+
+        relativeOrientations = self.getRelativeOrientations()
+        thumbJ0X = (relativeOrientations[0] - self.thumb.getJ1Flex() + 360) % 360
+        thumbJ0Y = relativeOrientations[1]
+        thumbJ0Z = relativeOrientations[2]
+
+        pointerJ0X = (relativeOrientations[3] - self.pointer.getJ1Flex() - self.pointer.getJ2Flex() + 360) % 360
+        pointerJ0Y = relativeOrientations[4]
+        pointerJ0Z = relativeOrientations[5]
+
+        #pointerJ0X = (self.pointerGyroX - self.pointer.getJ1Flex() - self.pointer.getJ2Flex() + 360) % 360
+        #pointerJ0Y = self.pointerGyroY
+        #pointerJ0Z = self.pointerGyroZ
+
+        middleJ0X = (relativeOrientations[6] - self.middle.getJ1Flex() - self.middle.getJ2Flex() + 360) % 360
+        middleJ0Y = relativeOrientations[7]
+        middleJ0Z = relativeOrientations[8]
+
+        ringJ0X = (relativeOrientations[9] - self.ring.getJ1Flex() - self.ring.getJ2Flex() + 360) % 360
+        ringJ0Y = relativeOrientations[10]
+        ringJ0Z = relativeOrientations[11]
+
+        pinkyJ0X = (relativeOrientations[12] - self.pinky.getJ1Flex() - self.pinky.getJ2Flex() + 360) % 360
+        pinkyJ0Y = relativeOrientations[13]
+        pinkyJ0Z = relativeOrientations[14]
+
+        j0Angles = [thumbJ0X, thumbJ0Y, thumbJ0Z, pointerJ0X, pointerJ0Y, pointerJ0Z, middleJ0X, middleJ0Y, middleJ0Z,
+                    ringJ0X, ringJ0Y, ringJ0Z, pinkyJ0X, pinkyJ0Y, pinkyJ0Z]
+        return j0Angles
 
 
 
